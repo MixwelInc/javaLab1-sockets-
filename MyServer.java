021e50f359beb607470c81b968901aead37180dc
snuser <smakar20001@gmail.com>
2022-10-01 20:06:40 +0300

    small changes

--- MyServer.java
+++ MyServer.java
@@ -5,14 +5,16 @@ import java.io.*;
 class MyServer{  
     public static void main(String args[])throws Exception {  
         ServerSocket ss=new ServerSocket(3333);  
+        System.out.print("Waiting for client\n");
         Socket s=ss.accept();  
+        System.out.print("Connection established\n");
         DataInputStream din=new DataInputStream(s.getInputStream());  
         DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-        boolean lampState = false;
+        boolean boilerState = false;
 
-        String clMsg="",answer="",welcomeStr = "Hello!\nThe lamp is turned off.\nType /help to see the commands.\n",
-        helpStr = "To turn off the lamp type -off\nTo turn on the lamp type -on\nTo stop server type /stop\n",
-        gbStr = "The server is turned off\n"; 
+        String clMsg="",answer="",welcomeStr = "Hello!\nThe boiler is turned off.\nType /help to see the commands.\n",
+        helpStr = "To turn off the boiler type -off\nTo turn on the boiler type -on\nTo stop server type /stop\n",
+        gbStr = "The server is turned off\n";
 
         dout.writeUTF(welcomeStr);
         dout.flush();
@@ -27,23 +29,25 @@ class MyServer{
             else if(clMsg.equals("/help")) {                             //if /help send helpMsg
                 answer = helpStr;
             }
-            else if(clMsg.equals("-on") && !lampState ) {
-                lampState = true;
-                answer = "The lamp is now turned on\n";
+            else if(clMsg.equals("-on") && !boilerState ) {
+                boilerState = true;
+                answer = "The boiler is now turned on\n";
             }
-            else if(clMsg.equals("-on") && lampState ) {
-                answer = "The lamp is already turned on\n";
+            else if(clMsg.equals("-on") && boilerState ) {
+                answer = "The boiler is already turned on\n";
             }
-            else if(clMsg.equals("-off") && !lampState ) {
-                answer = "The lamp is already turned off\n";
+            else if(clMsg.equals("-off") && !boilerState ) {
+                answer = "The boiler is already turned off\n";
             }
-            else if(clMsg.equals("-off") && lampState ) {
-                answer = "The lamp is now turned off\n";
-                lampState = false;
+            else if(clMsg.equals("-off") && boilerState ) {
+                answer = "The boiler is now turned off\n";
+                boilerState = false;
             }
             //clMsg=din.readUTF().toString(); 
             dout.writeUTF(answer);
             dout.flush();                                           //writing buffered answer
         }
+        s.close();
+        ss.close();
     }
 }  
